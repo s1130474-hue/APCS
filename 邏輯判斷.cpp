b74128419a7f84ccc980ad16c673d8ae7c3b9157
s1130474-hue <s1130474@dmhs.kh.edu.tw>
2026-03-10 11:55:29 +0800

    Create 邏輯判斷.cpp

---
+++ 邏輯判斷.cpp
@@ -0,0 +1,20 @@
+//============================================================================
+// Name        : apcs.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << (5>=2) << endl;
+	cout << (5<=2) << endl;
+	cout << (5>2) << endl;
+	cout << (5<2) << endl;
+	cout << (5==2) << endl;
+	cout << (5!=2) << endl;
+	return 0;
+}
