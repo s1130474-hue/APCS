3985254c90a79d77531d0f3eca0863d60c587cad
s1130474-hue <s1130474@dmhs.kh.edu.tw>
2026-03-24 11:43:50 +0800

    Eva 的回家作業.cpp

--- Eva 的回家作業.cpp
+++ Eva 的回家作業.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-	int N;
-	cin >> N;
-	for (int i=1; i<N; i+=2) {
-		cout << i << endl;
+	int t, a0, a1, a2, a3, a4;
+	cin >> t;
+	for (int i=0; i<t; i++){
+		cin >> a0>> a1>> a2>> a3;
+		if((a1-a0)==(a2-a1)){
+			a4 = a3 + (a2-a1);
+		}else {
+			a4 = a3 * (a2/a1);
+		}
+		cout << a0 <<" "<< a1 <<" "<< a2 <<" "<< a3 <<" "<< a4 <<" "
 	}
 	return 0;
 }
