55a11fdb0f471771e7e1ee6c10f98d73c68c49c7
s1130474-hue <s1130474@dmhs.kh.edu.tw>
2026-04-07 10:47:03 +0800

    Create MOD3.cpp

---
+++ MOD3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+
+	int x, arr[3]; // arr[0] arr[1] arr[2]
+	for (int i=0;i<n;i++){
+		cin >> x;
+		arr[x % 3]++;
+	}
+	cout << arr[0] << " " arr[1] << " " arr[2] << " " << endl;
+	return 0;
+}
