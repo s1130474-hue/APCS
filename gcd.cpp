e7776f2cc657c8b66d80ecdfc4ed80142aaabab5
s1130474-hue <s1130474@dmhs.kh.edu.tw>
2026-04-14 10:54:21 +0800

    Create gcd.cpp

---
+++ gcd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	cin >> x >> y;
+	while (x % y != 0){
+		int temp = x % y;
+		x = y;
+		y = temp;
+	}
+	cout << y << endl;
+	return 0;
+}
