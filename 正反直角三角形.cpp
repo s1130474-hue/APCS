21b2b7f69a657e091a600b58abe9da129665d198
s1130474-hue <s1130474@dmhs.kh.edu.tw>
2026-04-21 10:48:00 +0800

    Create 正反直角三角形.cpp

---
+++ 正反直角三角形.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "-- 正直角三角形 --\n";
+	for(int i = 1; i <= 5; i++){
+		for(int j = 1; j <= i; j++){
+			cout  << "*";
+		}
+		cout << "\n";
+	}
+	cout << "-- 反直角三角形 --\n";
+		for(int i = 5; i <= 1; i--){
+			for(int j = 1; j <= i; j++){
+				cout  << "*";
+			}
+			cout << "\n";
+		}
+		return 0;
+	}
